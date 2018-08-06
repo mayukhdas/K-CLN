279f890e89d8fc8aff8be0e612bab9738ba5bd55
Yang Yu <yang@cometnet-10-21-2-72.utdallas.edu>
2018-08-06 16:18:44 -0500

    Sampling

---
+++ code/full_batch/sample_data.py
@@ -0,0 +1,54 @@
+from prepare_data import modData
+from prepare_data import create_mask
+import gzip
+import cPickle
+import numpy
+import read_pkl as rp
+import random
+
+path = '../data/' + 'pubmed' + '.pkl.gz'
+
+
+def sample_data(path, portion):
+    f = gzip.open(path, 'rb')
+    # print(cPickle.load(f))
+    feats, labels, rel_list, train_ids, valid_ids, test_ids = cPickle.load(f)
+    feats, labels = modData(feats,labels)
+
+    train_ids_sample = random.sample(train_ids, int(len(train_ids)*portion))
+    valid_ids_sample = random.sample(valid_ids, int(len(valid_ids)*portion))
+    test_ids_sample = random.sample(test_ids, int(len(test_ids)*portion))
+    index = list()
+    index.extend(train_ids_sample)
+    index.extend(valid_ids_sample)
+    index.extend(test_ids_sample)
+    feats_sample = feats[index]
+    labels_sample = labels[index]
+    rel_list_sample = list()
+    for rel in [rel_list[i] for i in index]:
+
+        rel_item_0 = list()
+        for item_index in rel[0]:
+            if item_index in index:
+                rel_item_0.append(item_index)
+        rel_item_1 = list()
+        for item_index in rel[1]:
+            if item_index in index:
+                rel_item_1.append(item_index)
+        rel_item_tuple = list()
+        rel_item_tuple.append(rel_item_0)
+        rel_item_tuple.append(rel_item_1)
+        # print rel_item_tuple
+        rel_list_sample.append(rel_item_tuple)
+
+
+
+    # print len(rel_list)
+
+
+    rel_list, rel_mask = create_mask(rel_list)
+    return
+
+    # return feats_sample, labels_sample, rel_list_sample, rel_mask_sample, train_ids_sample, valid_ids_sample, test_ids_sample
+
+sample_data(path, 0.1)
