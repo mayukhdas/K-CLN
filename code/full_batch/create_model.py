a6aa949e0cdde3054be901694744dde777e3e4a2
Mayukh Das <mayukhds@gmail.com>
2018-08-08 11:13:33 -0500

    Adding comments

--- code/full_batch/create_model.py
+++ code/full_batch/create_model.py
@@ -258,6 +258,7 @@ def create_dense(n_layers, hidden_dim, input_dim, n_rel, n_neigh, n_classes, sha
     inp_nodes = Input(shape=(input_dim,), dtype='float32', name='inp_nodes')
     inp_rel = Input(shape=(n_rel, n_neigh), dtype='int64', name='inp_rel')
     inp_rel_mask = Input(shape=(2, n_rel, n_neigh), dtype='float32', name='inp_rel_mask')
+    #additional parameters for advice 
     inp_I_adv = Input(shape=(input_dim,), dtype='float32', name='inp_I_adv')
     inp_W_adv_mask = Input(shape=(input_dim,), dtype='float32', name='inp_W_adv_mask')
     inp_c_adv_mask = Input(shape=(n_rel, n_neigh), dtype='float32', name='inp_c_adv_mask')
@@ -283,12 +284,12 @@ def create_dense(n_layers, hidden_dim, input_dim, n_rel, n_neigh, n_classes, sha
         #hidd_nodes = GraphDense(input_dim=hidden_dim, output_dim=hidden_dim, init=init,
                                 #n_rel=n_rel, mean=nmean, activation=act)([hidd_nodes, inp_rel, inp_rel_mask])
         hidd_nodes = GraphDense(input_dim=hidden_dim, output_dim=hidden_dim, init=init, prefEffect = tempTop,
-                                n_rel=n_rel, mean=nmean, activation=act)([hidd_nodes, inp_rel, inp_rel_mask, inp_I_adv, inp_W_adv_mask, inp_c_adv_mask])
+                                n_rel=n_rel, mean=nmean, activation=act)([hidd_nodes, inp_rel, inp_rel_mask, inp_I_adv, inp_W_adv_mask, inp_c_adv_mask]) # changes by MD & Yang
         if dropout: hidd_nodes = Dropout(0.5)(hidd_nodes)
 
     top_nodes = Dense(output_dim=n_classes, input_dim=hidden_dim)(hidd_nodes)
     top_nodes = Activation(activation=top_act)(top_nodes)
-    model = Model(input=[inp_nodes, inp_rel, inp_rel_mask, inp_I_adv, inp_W_adv_mask, inp_c_adv_mask], output=[top_nodes])
+    model = Model(input=[inp_nodes, inp_rel, inp_rel_mask, inp_I_adv, inp_W_adv_mask, inp_c_adv_mask], output=[top_nodes]) # changes by MD & Yang
 
     return model
 
