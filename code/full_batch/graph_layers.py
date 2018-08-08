774f96d77c672adbff23637792416fad9c65322c
Yang Yu <yang@cometnet-10-21-57-244.utdallas.edu>
2018-08-07 19:53:17 -0500

    print

--- code/full_batch/graph_layers.py
+++ code/full_batch/graph_layers.py
@@ -229,9 +229,6 @@ class GraphDense(Layer):
         return (None, self.output_dim)
 
     def call(self, inputs, mask=None):
-        print(inputs[0].shape)
-        print(inputs[1].shape)
-        print(inputs[2].shape)
         x = inputs[0] #feature matrix
         # print("x in the call ",x.shape)
         rel = inputs[1] # n_nodes, n_rel, n_neigh
