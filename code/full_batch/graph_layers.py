54a218c86df27773d0db6830d3e4d7e35269c4b1
Mayukh Das <mayukhds@gmail.com>
2018-08-18 18:31:18 -0500

    fix 3

--- code/full_batch/graph_layers.py
+++ code/full_batch/graph_layers.py
@@ -263,7 +263,7 @@ class GraphDense(Layer):
         #print("final Layer **** ", fla.fprobs)
         context = K.sum(context, axis=-2) / K.sum(mask_div, axis=-1)[:, :, None]
         # Calculate indicator ---- MD
-        if not (True in K.tensor.isnan(yprobs)):
+        if not (True in np.isnan(K.eval(yprobs))):
             advice_gate = K.sum((y_adv_mask - yprobs)*y_adv_mask)
             #context = context * np.exp(np.dot(np.subtract(Iadv[:],self.prefEffect[:]),c_adv_mask[:, :, :, None]))##doing - MD+DEV+YANG
             context = context * K.exp(advice_gate*c_adv_mask[:, :, :, None])
