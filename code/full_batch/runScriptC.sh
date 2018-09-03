951077186d4c7de484b45de2da82ac76b28bf0ff
Mayukh Das <mayukhds@gmail.com>
2018-09-03 15:02:52 -0500

    update script

--- code/full_batch/runScriptC.sh
+++ code/full_batch/runScriptC.sh
@@ -1,2 +1,2 @@
-KERAS_BACKEND=theano python trainingD.py -model Dense -data corporate -dim 40 -nlayers 10 -nmean 540 -y 0 -sample "../data/corporateSample40P.json"
+KERAS_BACKEND=theano python trainingC.py -model Dense -data corporate -dim 40 -nlayers 10 -nmean 540 -y 0 -sample "../data/corporateSample40P.json"
 
