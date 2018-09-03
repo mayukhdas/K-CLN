13080b18e92d59a5a221cbab40154afa68b301ad
Mayukh Das <mayukhds@gmail.com>
2018-09-03 17:38:56 -0500

    some advice changes

--- code/full_batch/adviceFileC.py
+++ code/full_batch/adviceFileC.py
@@ -9,7 +9,7 @@ adviceSet = [{'h':[['label','?e','2']], 'b':[['hasWord','?e', 'look']]}, #advice
              {'h':[['label','?e','2']], 'b':[['hasWord','?e', 'check']]}, #advice2
              {'h':[['label','?e','2']], 'b':[['hasWord','?e', 'stay']]}, #advice3
              {'h':[['label','?e','2']], 'b':[['hasWord','?e', 'find']]}, #advice3
-             {'h':[['label','?e','2']], 'b':[['hasWord','?e2', 'find out'],['sameauth','?e','?e2']]}, #advice4 
-             {'h':[['label','?e','2']], 'b':[['hasWord','?e2', 'check'],['sameauth','?e','?e2']]}, #advice4 
+             {'h':[['label','?e','1']], 'b':[['hasWord','?e2', 'find out'],['sameauth','?e','?e2']]}, #advice4 
+             {'h':[['label','?e','1']], 'b':[['hasWord','?e2', 'check'],['sameauth','?e','?e2']]}, #advice4 
              ]
 
