f62a3e01563efd0babf0acc784c91fac70517059
Mayukh Das <mayukhds@gmail.com>
2018-09-19 01:15:25 -0500

    runscript update

--- code/full_batch/runScriptSsample.sh
+++ code/full_batch/runScriptSsample.sh
@@ -1,18 +1,18 @@
-KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 20 -y 0 -sample "../data/socialSample5P.json"
+KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 5 -y 0 -sample "../data/socialSample5P.json"
 mv log/pubmed.txt log/social3Pad.txt
 
-KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 20 -y 0 -sample "../data/socialSample10P.json"
+KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 5 -y 0 -sample "../data/socialSample10P.json"
 mv log/pubmed.txt log/social6Pad.txt
 
-KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 20 -y 0 -sample "../data/socialSample20P.json"
+KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 5 -y 0 -sample "../data/socialSample20P.json"
 mv log/pubmed.txt log/social12Pad.txt
 
-KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 20 -y 0 -sample "../data/socialSample60P.json"
+KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 5 -y 0 -sample "../data/socialSample60P.json"
 mv log/pubmed.txt log/social36Pad.txt
 
-KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 20 -y 0 -sample "../data/socialSample80P.json"
+KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 5 -y 0 -sample "../data/socialSample80P.json"
 mv log/pubmed.txt log/social48Pad.txt
 
-KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 20 -y 0 -sample "../data/socialSample100P.json"
+KERAS_BACKEND=theano python trainingS.py -model Dense -data social -dim 40 -nlayers 10 -nmean 5 -y 0 -sample "../data/socialSample100P.json"
 mv log/pubmed.txt log/social60Pad.txt
 
--- code/full_batch/testing.py
+++ code/full_batch/testing.py
@@ -14,3 +14,17 @@ feats, labels, rel_list, train_ids, valid_ids, test_ids = pkl.load(f)
 
 print [i for i,x in enumerate(labels) if x>0]
 
+avg1 = 0.0
+c1 = 0
+avg2 = 0.0
+c2 = 0
+for x in rel_list:
+    if len(x[0])>0:
+        avg1 = avg1 + len(x[0])
+        c1 = c1+1
+    if len(x[1])>0:
+        avg2 = avg2 + len(x[1])
+        c2 = c2+1
+
+print (avg1/float(c1)), (avg2/float(c2))
+
