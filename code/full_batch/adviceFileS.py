9a12971748a2f489bebb649b67a90fa314d16f5d
Mayukh Das <mayukhds@gmail.com>
2018-09-19 15:30:11 -0500

    Change in advice

--- code/full_batch/adviceFileS.py
+++ code/full_batch/adviceFileS.py
@@ -9,7 +9,8 @@ adviceSet = [{'h':[['label','?e','1']], 'b':[['hasWord','?e', 'die']]}, #advice1
              {'h':[['label','?e','1']], 'b':[['hasWord','?e', 'dead']]}, #advice2
              {'h':[['label','?e','1']], 'b':[['hasWord','?e', 'kill']]}, #advice3
              {'h':[['label','?e','1']], 'b':[['hasWord','?e', 'disaster']]}, #advice3
-             {'h':[['label','?e','1']], 'b':[['hasWord','?e2', 'kill'],['sameLoc','?e','?e2']]}, #advice4 
+             {'h':[['label','?e','0']], 'b':[['hasWord','?e2', 'check'],['sameUser','?e','?e2']]}, #advice4 
+             {'h':[['label','?e','0']], 'b':[['hasWord','?e2', 'offer'],['sameUser','?e','?e2']]}, #advice4 
              {'h':[['label','?e','1']], 'b':[['hasWord','?e2', 'disaster'],['sameUser','?e','?e2']]}, #advice4 
              ]
 
